41d54db581d2341f4e1cf1fff098a954ec69fc6b
Vijay Selvaraj <vijayvj7094@gmail.com>
2023-10-03 13:49:40 +0530

    done by vijay

--- src/com/git/GIT.java
+++ src/com/git/GIT.java
@@ -3,8 +3,16 @@ package com.git;
 public class GIT {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("JAVA FULL STACK DEVELOPERS");
+		
+		String obj = "WELCOME";
+
+		for (int i = 0; i < obj.length(); i++) {
+			System.out.println(obj.charAt(i));
+
+		}
+
 	}
 
 }
